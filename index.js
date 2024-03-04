f452f5af4a5cb908acd943e5db6405ca80a86b13
RAGHAW PATHAK <raghawpathak001@gmail.com>
2024-03-05 00:08:23 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+// Select all elements with the class 'panel' and store them in the 'panels' variable
+const panels = document.querySelectorAll('.panel');
+
+// Add a click event listener to each panel
+panels.forEach((panel) => {
+    panel.addEventListener('click', () => {
+        // When a panel is clicked, remove the 'active' class from all panels
+        removeClassActive();
+        // Add the 'active' class to the clicked panel
+        panel.classList.add('active');
+    });
+});
+
+// Function to remove the 'active' class from all panels
+const removeClassActive = () => {
+    // Iterate through all panels and remove the 'active' class
+    panels.forEach((panel) => {
+        panel.classList.remove('active');
+    });
+}
